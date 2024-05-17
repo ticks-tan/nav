0cdb1aa710ebdb198b0ccfd2850981f571565fc9
Ticks <ticks.cc@gmail.com>
2024-05-18 00:06:50 +0800

    [add]: 初始化项目

---
+++ src/types/category.ts
@@ -0,0 +1,9 @@
+/**
+ * 网站分类
+ */
+
+export type SiteCategory = {
+    id: string,
+    // 分类名称
+    name: string,
+};
---
+++ src/types/site.ts
@@ -0,0 +1,15 @@
+/**
+ * 网站
+ */
+
+export type SiteItem = {
+    id: string,
+    // 网站名称
+    name: string,
+    // 网站网址
+    url: string,
+    // 网站描述
+    desc: string,
+    // 网站 Logo
+    logo: string,
+};
